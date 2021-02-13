8c3077e7bc0259285dac26a778a4ffd18add6927
saeed <saeed@pop-os.localdomain>
2021-02-14 01:53:13 +0330

    mobile template and home template are modifiled

--- site_view/views.py
+++ site_view/views.py
@@ -11,16 +11,21 @@ class SliderView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["slides"] = Slider.objects.filter(status=True)
-        mobile_category = Category.objects.get(name="گوشی موبایل")
-        mobile_phones = ShopProduct.objects.filter(product__category=mobile_category)
-        context['mobiles'] = mobile_phones
+        mobile_phones = Product.objects.filter(category__slug='mobile-phone').order_by('created')[:5]
+        context['mobiles'] = []
+        for product in mobile_phones:
+            context['mobiles'].append(
+                ShopProduct.objects.filter(product=product).first())
+
         context['festival'] = Advertisement.objects.filter(status=True, priority='festival').first()
         context['highPriorityAdv'] = Advertisement.objects.filter(status=True, priority='high-adv')[:2]
         context['foursomeAdv'] = Advertisement.objects.filter(status=True, priority='foursome-adv')
         context['twosomeAdv'] = Advertisement.objects.filter(status=True, priority='twosome-adv')
-        laptop_category = Category.objects.get(name='لپ تاپ')
-        laptops = ShopProduct.objects.filter(product__category=laptop_category)
-        context['laptops'] = laptops
+        laptops = Product.objects.filter(category__slug="laptop").order_by('created')[:5]
+        context['laptops'] = []
+        for product in laptops:
+            context['laptops'].append(ShopProduct.objects.filter(product=product).first())
+        print(context['laptops'])
         context['logos'] = Logo.objects.get(title='logo')
         return context
 
